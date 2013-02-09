4c3b24bbad7994b6bb98829286318313ca8bb19d
Jeff Steinport <jeff.steinport@gmail.com>
2013-02-09 17:57:33 -0500

    Update pbkdf2.php

--- pbkdf2.php
+++ pbkdf2.php
@@ -7,30 +7,40 @@
      *  @param int kl derived key length
      *  @param string a hash algorithm (defaults to whirlpool)
      *
+     *  Returns binary output that can be used to encryption keys.
+     *  Use base64_encode to output plain text.
      *  @return string derived key
+     *  
+     *  Example:
+     *  $key = pbkdf2("tk421","nD3$df9aSdVV0pr@dv**",5000,32);
+     *  echo base64_encode($key);
+     *  returns "0IMN1zq83irlxuhuRJLs6VlZEnB2m231QO5gTgSP0MI="
+     * 
     */
-    function pbkdf2( $p, $s, $c, $kl, $a = 'whirlpool' ) {
+
+function pbkdf2( $p, $s, $c, $kl, $a = 'whirlpool' ) {
      
-        $hl = strlen(hash($a, null, true)); # Hash length
-        $kb = ceil($kl / $hl);              # Key blocks to compute
-        $dk = '';                           # Derived key
+    $hl = strlen(hash($a, null, true)); # Hash length
+    $kb = ceil($kl / $hl);              # Key blocks to compute
+    $dk = '';                           # Derived key
      
-        # Create key
-        for ( $block = 1; $block <= $kb; $block ++ ) {
+    # Create key
+    for ( $block = 1; $block <= $kb; $block ++ ) {
      
-            # Initial hash for this block
-            $ib = $b = hash_hmac($a, $s . pack('N', $block), $p, true);
+        # Initial hash for this block
+        $ib = $b = hash_hmac($a, $s . pack('N', $block), $p, true);
      
-            # Perform block iterations
-            for ( $i = 1; $i < $c; $i ++ )
+        # Perform block iterations
+        for ( $i = 1; $i < $c; $i ++ )
      
-                # XOR each iterate
-                $ib ^= ($b = hash_hmac($a, $b, $p, true));
+            # XOR each iterate
+            $ib ^= ($b = hash_hmac($a, $b, $p, true));
      
-            $dk .= $ib; # Append iterated block
-        }
-     
-        # Return derived key of correct length
-        return substr($dk, 0, $kl);
+        $dk .= $ib; # Append iterated block
     }
+     
+    # Return derived key of correct length
+    return substr($dk, 0, $kl);
+}
+
 ?>
